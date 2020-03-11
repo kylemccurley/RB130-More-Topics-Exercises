748abe41caa8cfc7d7a283723a3e5648a775134b
Kyle McCurley <kmccurley123@gmail.com>
2020-03-11 19:28:03 -0400

    Update rot13.rb
    Changed the implementation of the two methods, as well as the duties charged to them.

--- rot_13.rb
+++ rot_13.rb
@@ -1,41 +1,40 @@
-ENCRYPTED_PIONEERS = 
-  [
-    'Nqn Ybirynpr',
-    'Tenpr Ubccre',
-    'Nqryr Tbyqfgvar',
-    'Nyna Ghevat',
-    'Puneyrf Onoontr',
-    'Noqhyynu Zhunzznq ova Zhfn ny-Xujnevmzv',
-    'Wbua Ngnanfbss',
-    'Ybvf Unvog',
-    'Pynhqr Funaaba',
-    'Fgrir Wbof',
-    'Ovyy Tngrf',
-    'Gvz Orearef-Yrr',  
-    'Fgrir Jbmavnx',
-    'Xbaenq Mhfr',
-    'Fve Nagbal Ubner',
-    'Zneiva Zvafxl',
-    'Lhxvuveb Zngfhzbgb',
-    'Unllvz Fybavzfxv',
-    'Tregehqr Oynapu'
+ENCRYPTED_PIONEERS = [
+  'Nqn Ybirynpr',
+  'Tenpr Ubccre',
+  'Nqryr Tbyqfgvar',
+  'Nyna Ghevat',
+  'Puneyrf Onoontr',
+  'Noqhyynu Zhunzznq ova Zhfn ny-Xujnevmzv',
+  'Wbua Ngnanfbss',
+  'Ybvf Unvog',
+  'Pynhqr Funaaba',
+  'Fgrir Wbof',
+  'Ovyy Tngrf',
+  'Gvz Orearef-Yrr',
+  'Fgrir Jbmavnx',
+  'Xbaenq Mhfr',
+  'Fve Nagbal Ubner',
+  'Zneiva Zvafxl',
+  'Lhxvuveb Zngfhzbgb',
+  'Unllvz Fybavzfxv',
+  'Tregehqr Oynapu'
 ]
 
-def decipher_words
-  output = ENCRYPTED_PIONEERS.map do |word|
-             word.chars.map do |char|
-               decipher_char(char)
-             end
-           end
- 
-  output.map { |word| word.join }
-end
-
-def decipher_char(char)
-  case char
-  when 'A'..'M', 'a'..'m' then (char.ord + 13).chr
-  when 'N'..'Z', 'n'..'z' then (char.ord - 13).chr
+def rot13(word)
+  output_word = ''
+  word.each_char do |char|
+    case char
+    when 'A'..'M', 'a'..'m' then output_word += (char.ord + 13).chr
+    when 'n'..'z', 'N'..'Z' then output_word += (char.ord - 13).chr
+    end
   end
+  output_word
 end
 
-puts decipher_words
+def decipher
+  output = []
+  ENCRYPTED_PIONEERS.each { |word| output << rot13(word) }
+  output
+end
+  
+puts decipher
